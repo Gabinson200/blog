f73ef9b60dcf57ace5d40a63f72702e5063bac9b
Gabinson200 <42674620+Gabinson200@users.noreply.github.com>
2025-09-17 12:02:47 -0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  darkMode: 'class',
-  content: ["src/**/*.{njk,md,html}"],
-  theme: { extend: {} },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
